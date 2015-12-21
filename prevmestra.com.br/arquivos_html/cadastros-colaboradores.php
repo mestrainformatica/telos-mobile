58d5fba79d48df4bac8e653ec72156142567ef85
Anthony Sousa <anthony.acsousa@gmail.com>
2015-12-21 12:13:15 -0200

    Cadastros Colaboradores - Aba 2

--- prevmestra.com.br/arquivos_html/cadastros-colaboradores.php
+++ prevmestra.com.br/arquivos_html/cadastros-colaboradores.php
@@ -950,7 +950,7 @@ msgTeclaRedefinida = 'Esta função foi redefinida para a tecla "{0}"';
 			    	<li class="ui-state-default ui-corner-top" role="tab" tabindex="-1" aria-controls="tabs-19" aria-labelledby="a-tab19" aria-selected="false" aria-expanded="false"><a id="a-tab19" href="http://www.sysprev.com.br/FAECES_FolhaAtivo/rubricasman.do?evento=x&amp;idRotina=4097#tabs-19" onclick="mudaAba(&#39;3&#39;);" class="ui-tabs-anchor" role="presentation" tabindex="-1">Centro de Custo</a></li>	
 			  	</ul>
 					<div id="tabs-1" aria-labelledby="a-tab1" class="ui-tabs-panel ui-widget-content ui-corner-bottom" role="tabpanel" aria-hidden="false" style="display: block;">
-						<?php include("_includes/colaboradores/tabs-1.html"); ?>	
+						<?php include("_includes/colaboradores/tabs-2.html"); ?>	
 					</div>
 					<div id="tabs-2" aria-labelledby="a-tab2" class="ui-tabs-panel ui-widget-content ui-corner-bottom" role="tabpanel" aria-hidden="true" style="display: none;">
 						<?php include("_includes/colaboradores/tabs-2.html"); ?>
