78be346dd02411b95c596d57c81ea8eb6354789a
Richard Barros <richard@homemmaquina.com.br>
2016-07-08 12:27:09 -0300

    Atualizações de formulário

--- fapes/lib/js/main.js
+++ fapes/lib/js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
 
-	$('#btnExcluir').on('click', function(){
-		$('.table-values').first().addClass('danger');
+	$('.btnExcluir').on('click', function(){
+		
+		$tr = $(this).parent().parent();
+
+		if ($tr.hasClass('danger')){
+			$tr.removeClass('danger');
+			$tr.find('button').toggleClass('btn-danger').toggleClass('btn-default').text('Remover');
+		} else {
+			$tr.addClass('danger');
+			$tr.find('button').toggleClass('btn-danger').toggleClass('btn-default').text('Desfazer');
+		}
+
+		if($tr.next().hasClass('oculto')){
+			$tr.next().removeClass('oculto');
+		}else{
+			$tr.next().addClass('oculto');
+		}
+
 	});
+
 
 });
