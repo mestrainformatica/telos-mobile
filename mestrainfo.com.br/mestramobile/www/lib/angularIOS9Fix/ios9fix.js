61fd6156c2a2c8349e039587b69484a4bbc0ada5
Mestra <mestra@mestrainfo.com.br>
2017-03-29 17:41:15 -0300

    Prevmobile | Atualização de RMV+Saque+Envio de email de Telos=>Mestrainfo

--- mestrainfo.com.br/mestramobile/www/lib/angularIOS9Fix/ios9fix.js
+++ mestrainfo.com.br/mestramobile/www/lib/angularIOS9Fix/ios9fix.js
@@ -1,44 +1,10 @@
-/**
- * ==================  angular-ios9-uiwebview.patch.js v1.1.1 ==================
- *
- * This patch works around iOS9 UIWebView regression that causes infinite digest
- * errors in Angular.
- *
- * The patch can be applied to Angular 1.2.0 – 1.4.5. Newer versions of Angular
- * have the workaround baked in.
- *
- * To apply this patch load/bundle this file with your application and add a
- * dependency on the "ngIOS9UIWebViewPatch" module to your main app module.
- *
- * For example:
- *
- * ```
- * angular.module('myApp', ['ngRoute'])`
- * ```
- *
- * becomes
- *
- * ```
- * angular.module('myApp', ['ngRoute', 'ngIOS9UIWebViewPatch'])
- * ```
- *
- *
- * More info:
- * - https://openradar.appspot.com/22186109
- * - https://github.com/angular/angular.js/issues/12241
- * - https://github.com/driftyco/ionic/issues/4082
- *
- *
- * @license AngularJS
- * (c) 2010-2015 Google, Inc. http://angularjs.org
- * License: MIT
- */
-
 angular.module('ngIOS9UIWebViewPatch', ['ng']).config(['$provide', function($provide) {
   'use strict';
 
   $provide.decorator('$browser', ['$delegate', '$window', function($delegate, $window) {
 
+
+    console.log('entrou aqui #1');
     if (isIOS9UIWebView($window.navigator.userAgent)) {
       return applyIOS9Shim($delegate);
     }
@@ -67,6 +33,7 @@ angular.module('ngIOS9UIWebViewPatch', ['ng']).config(['$provide', function($pro
 
       function clearPendingLocationUrl() {
         pendingLocationUrl = null;
+        console.log('entrou aqui #2');
       }
 
       return browser;
