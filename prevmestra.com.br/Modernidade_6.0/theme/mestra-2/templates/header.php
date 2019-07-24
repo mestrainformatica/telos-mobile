2c389e0ea826eff6bd32ec348a08c1aab68948b6
Madalena <madalena.lpessanha@gmail.com>
2019-07-24 12:14:59 -0300

    dropdown menu header

--- prevmestra.com.br/Modernidade_6.0/theme/mestra-2/templates/header.php
+++ prevmestra.com.br/Modernidade_6.0/theme/mestra-2/templates/header.php
@@ -390,24 +390,6 @@
 									</div>
 									<div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-xl">
 
-										<!--begin: Head -->
-										<div class="kt-user-card kt-user-card--skin-light kt-notification-item-padding-x">
-											<div class="kt-user-card__avatar">
-												<img class="kt-hidden-" alt="Pic" src="mestra-2/assets/media/users/300_25.jpg" />
-
-												<!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
-												<span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold kt-hidden">R</span>
-											</div>
-											<div class="kt-user-card__name">
-												Richard Barros
-											</div>
-											<div class="kt-user-card__badge">
-												<span class="btn btn-label-primary btn-sm btn-bold btn-font-md">23 mensagens</span>
-											</div>
-										</div>
-
-										<!--end: Head -->
-
 										<!--begin: Navigation -->
 										<div class="kt-notification">
 											<a href="#" class="kt-notification__item">
@@ -416,10 +398,10 @@
 												</div>
 												<div class="kt-notification__item-details">
 													<div class="kt-notification__item-title kt-font-bold">
-														My Profile
+														Minha conta
 													</div>
 													<div class="kt-notification__item-time">
-														Account settings and more
+														Dados da conta e mais
 													</div>
 												</div>
 											</a>
@@ -429,10 +411,10 @@
 												</div>
 												<div class="kt-notification__item-details">
 													<div class="kt-notification__item-title kt-font-bold">
-														My mensagens
+														Minhas mensagens
 													</div>
 													<div class="kt-notification__item-time">
-														Inbox and tasks
+														Caixa de entrada e tarefas
 													</div>
 												</div>
 											</a>
@@ -442,10 +424,10 @@
 												</div>
 												<div class="kt-notification__item-details">
 													<div class="kt-notification__item-title kt-font-bold">
-														My Activities
+														Minhas atividades
 													</div>
 													<div class="kt-notification__item-time">
-														Logs and notifications
+														Logs e notificações
 													</div>
 												</div>
 											</a>
@@ -455,29 +437,16 @@
 												</div>
 												<div class="kt-notification__item-details">
 													<div class="kt-notification__item-title kt-font-bold">
-														My Tasks
+														Minhas tarefas
 													</div>
 													<div class="kt-notification__item-time">
-														latest tasks and projects
-													</div>
-												</div>
-											</a>
-											<a href="#" class="kt-notification__item">
-												<div class="kt-notification__item-icon">
-													<i class="flaticon2-cardiogram kt-font-warning"></i>
-												</div>
-												<div class="kt-notification__item-details">
-													<div class="kt-notification__item-title kt-font-bold">
-														Billing
-													</div>
-													<div class="kt-notification__item-time">
-														billing & statements <span class="kt-badge kt-badge--danger kt-badge--inline kt-badge--pill kt-badge--rounded">2 pending</span>
+														Últimas tarefas e projetos
 													</div>
 												</div>
 											</a>
 											<div class="kt-notification__custom kt-space-between">
-												<a href="demo11/custom/user/login-v2.html" target="_blank" class="btn btn-label btn-label-brand btn-sm btn-bold">Sign Out</a>
-												<a href="demo11/custom/user/login-v2.html" target="_blank" class="btn btn-clean btn-sm btn-bold">Upgrade Plan</a>
+												<a href="demo11/custom/user/login-v2.html" target="_blank" class="btn btn-label btn-label-brand btn-sm btn-bold">Desconectar</a>
+												
 											</div>
 										</div>
 
